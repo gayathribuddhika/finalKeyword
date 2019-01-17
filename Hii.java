b21108cc40b6b71e1b60902cdf116a9f053dbcfa
gayathribuddhika <39160904+gayathribuddhika@users.noreply.github.com>
2019-01-17 20:59:02 +0530

    FinalKeyword
    Final Keyword

---
+++ Hii.java
@@ -0,0 +1,14 @@
+
+public class Hii {
+       public final int number;
+    
+    public static void main(String[] args) {
+        Hii hi =new Hii();
+        //hi.number= 20; a final variable can only be initialized at once. 
+        
+    }
+    Hii(){
+     number = 10; 
+    
+    }
+}
